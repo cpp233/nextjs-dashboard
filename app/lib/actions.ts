3a6e39cae8346fb722ea0d13646bcc57f500ab13
cpp233 <tincint@foxmail.com>
2024-04-07 21:03:46 +0800

    通过 next-auth 鉴权

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -116,3 +116,27 @@ export async function deleteInvoice(id: string) {
     return { message: 'Database Error: Failed to Delete Invoice.' };
   }
 }
+
+// import * as authAction from '@/app/lib/actions/authAction';
+// export const authenticate = authAction.authenticate;
+
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
+}
